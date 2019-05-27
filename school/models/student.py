43cd4f9a50705ff02502b7ea04afaca3683efa4a
Nisarg_Patel <nisarg.p.serpentcs@gmail.com>
2019-05-27 12:23:48 +0530

    [IMP] removed if condition from _compute_full_name method

--- school/models/student.py
+++ school/models/student.py
@@ -29,17 +29,11 @@ class StudentStudent(models.Model):
     @api.depends('stu_name', 'middle', 'last')
     def _compute_full_name(self):
         for rec in self:
-            if rec.middle:
-                partner_name = ""
-                partner_name += rec.stu_name if rec.stu_name else ""
-                partner_name += " " + rec.middle if rec.middle else ""
-                partner_name += " " + rec.last if rec.last else ""
-                rec.full_name = partner_name
-            else:
-                partner_name = ""
-                partner_name += rec.stu_name if rec.stu_name else ""
-                partner_name += " " + rec.last if rec.last else ""
-                rec.full_name = partner_name
+            partner_name = ""
+            partner_name += rec.stu_name if rec.stu_name else ""
+            partner_name += " " + rec.middle if rec.middle else ""
+            partner_name += " " + rec.last if rec.last else ""
+            rec.full_name = partner_name
 
     @api.model
     def _search(self, args, offset=0, limit=None, order=None, count=False,
